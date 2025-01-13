261b0918b6dae8dc3ff2c15bc11535194b964903
Yasaman Karimi <yasamankarimi@Yasamans-MBP.localdomain>
2025-01-13 21:36:27 +0100

    add files for ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
-int main()
+
+void YellThings(std::string str)
 {
-    
+    for (size_t i = 0; i < str.size(); i++)
+    {
+        std::cout << (char)std::toupper(str[i]);
+    }
+}
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        std::cout<< "* LOUD AND UNBEARABLE FEEDBACK NOISE *"<< std::endl;
+    }
+    else
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            YellThings(argv[i]);
+        }
+        std::cout << std::endl;
+    }
+    return 0;
 }
